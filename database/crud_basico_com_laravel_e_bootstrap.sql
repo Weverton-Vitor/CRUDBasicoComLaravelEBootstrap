43c6922cca853de99c2c35e21b22d126db7b6946
Weverton Vitor <wevertonvitor01@gmail.com>
2019-12-24 12:13:11 -0300

    24 de Dezembro de 2019

---
+++ database/crud_basico_com_laravel_e_bootstrap.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA crud_basico_com_laravel_e_bootstrap;
+
+USE crud_basico_com_laravel_e_bootstrap;
+
+CREATE TABLE tipo(	
+	id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(100)
+);
+
+CREATE TABLE produtos(
+	id int PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(100) NOT NULL,
+    preco float(3) NOT NULL,    
+    marca varchar(100) NOT NULL,
+    tipo_id int,
+    data_fabricacao DATE NOT NULL,
+    data_validade DATE NOT NULL,
+    CONSTRAINT produto_tipo FOREIGN KEY(tipo_id) REFERENCES tipo(id)
+);
+
+
